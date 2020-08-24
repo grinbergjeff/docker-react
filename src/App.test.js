d86bedbbde89bdf48a8736f2b646d7ec5d61c63a
Jeffrey Grinberg <grinbergjeff@gmail.com>
2020-08-23 20:07:54 -0400

    testing feature branch

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/This should pass right?/i);
+  const linkElement = getByText(/Updated for feature/i);
   expect(linkElement).toBeInTheDocument();
 });
